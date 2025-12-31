1b0ebffcf4661064c932f7f8503836cea1c9b1c4
Mark Garrigan <mrkgrrgn@gmail.com>
2025-12-31 14:02:15 -0600

    first commit

---
+++ Casks/rated.rb
@@ -0,0 +1,11 @@
+cask "rated" do
+  version "0.0.0"
+  sha256 "REPLACE_ME"
+
+  url "https://github.com/plainwork/rated/releases/download/v#{version}/Rated-#{version}.zip"
+  name "Rated"
+  desc "Menu bar ratings"
+  homepage "https://github.com/plainwork/rated"
+
+  app "Rated.app"
+end
